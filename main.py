271c9580dd00b57ad709f7766f26be2ce943f047
glek <glek@berea.edu>
2024-03-12 01:28:38 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Welcome to the tip calculator!!")
+l = int(input("What was the total bill? $: "))
+
+j = int(input("How many people are splitting the bill?: "))
+
+k = int(input("How much tip would you like to give? 10, 12, or 15?: "))
+w = int(((100 + k )/ 100) * l)
+r = (round(w / j, 2))
+
+print(f"Each person should pay:  {r}")
